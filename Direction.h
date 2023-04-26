d04a2aa4f116d126417c160b33f7c9b618ffb1a9
Maya Raskin <mayaraskin14@gmail.com>
2023-04-26 21:22:27 +0300

    Removed redundant getDirectionString function

--- Direction.h
+++ Direction.h
@@ -1,23 +1,3 @@
 #pragma once
 
 enum class Direction : int{ UP, DOWN, LEFT, RIGHT, STAY};
-
-inline const char getDirectionString(Direction md) {
-    switch (md) {
-        case Direction::UP:
-            return 'U';
-            break;
-        case Direction::LEFT:
-            return 'L';
-            break;
-        case Direction::STAY:
-            return 'S';
-            break;
-        case Direction::RIGHT:
-            return 'R';
-            break;
-        case Direction::DOWN:
-            return 'D';
-            break;
-    }
-}
