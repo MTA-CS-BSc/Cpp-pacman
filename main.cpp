b7836bd7d4d3144772a95ad44e27a69bb69e205a
Maya Raskin <mayaraskin14@gmail.com>
2023-04-26 16:59:46 +0300

    Reformatted print

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ void startGame() {
 	Game game = Game();
 	game.start();
 
-	std::cout << "\nPress any key to go back to the menu" << std::endl;
+	std::cout << "\n\nPress any key to go back to the menu" << std::endl;
 	_getch();
 	clearScreen();
 }
