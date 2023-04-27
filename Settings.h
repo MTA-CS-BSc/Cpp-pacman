669861764c58fca8f80019e2c3b67be19bd3a157
Maya Raskin <mayaraskin14@gmail.com>
2023-04-27 23:34:23 +0300

    Updated speeds

--- Settings.h
+++ Settings.h
@@ -6,10 +6,12 @@
 class Settings {
 	public:
 		static constexpr int
-			PACMAN_SPEED = 2,
-			GHOST_SPEED = 1,
+			PACMAN_SPEED = 1,
 			BOARD_WIDTH = 80,
 			BOARD_HEIGHT = 25,
 			GHOSTS_AMOUNT = 2,
 			BREADCRUMBS_SCORE_CHANGE = 1;
+
+		static constexpr double
+			GHOST_SPEED = 0.5;
 };
