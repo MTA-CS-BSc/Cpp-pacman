f555047cba08108a59f8e36daff34ba1e91065ff
Maya Raskin <mayaraskin14@gmail.com>
2023-05-02 00:28:04 +0300

    Added comments for GameHandler, todo: movingEntity comments

--- GameHandler.h
+++ GameHandler.h
@@ -11,11 +11,20 @@ private:
     Pacman pacman;
     int breadcrumbs_amount;
 
-    bool isPacmanEaten();
-    void initPositions();
+    // Initializes the game board with breadcrumbs.
     void initializeBoard();
-    void resetBoard();
+
+    // Sets the inital on-board entities' positions.
+    void initPositions();
+
+    // Creates the ghosts objects according to the amount needed, declared in the settings module.
     void initGhosts();
+
+    // Returns true if the pacman was eaten, and false otherwise.
+    bool isPacmanEaten();
+
+    // Resets the entities positions and re-prints the board. 
+    void resetBoard();
 
 public:
     GameHandler();
@@ -28,8 +37,16 @@ public:
     inline void setLifes(int value) { this->lifes = value; }
     inline void setScore(int value) { this->score = value; }
     inline bool breadcrumbExists() { return this->breadcrumbs_amount != 0; }
+
+    // Adds 1 to the user's score if a breadcrumb was eaten
     void handleBreadcrumbsChange();
+
+    // If the pacman was eaten, removes a life from the pacman's remaining lifes
     void handlePacmanEaten();
+
+    // Prints the game board
     void printBoard();
+
+    // Changes the pacman's direction according to the pressed key
     void handlePacmanDirectionChange(Pacman& pacman, int key);
 };
