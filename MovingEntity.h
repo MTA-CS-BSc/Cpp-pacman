e9e2d65a3fdf2097a9e248f032701fbffbb6b095
Maya Raskin <mayaraskin14@gmail.com>
2023-05-02 00:33:45 +0300

    Added comments for MovingEntity

--- MovingEntity.h
+++ MovingEntity.h
@@ -11,13 +11,20 @@ class MovingEntity {
 
     protected:
         Point current_position;
+
+        // A virtual function to be implemented when inherting from MovingEntity
+        // that handles the calculation of the entity's new position (according to the entity's logic)
         virtual inline Point getNewPosition(std::vector<std::string>) { return Point();  }
+
+        // Handles tunneling (from one side to another).
         void tunnel(std::vector<std::string>, Point&);
         
-        inline bool isOnBoundary(std::vector<std::string> board, Point& p) {
+        // Returns true if the point is on a wall, and false otherwise.
+        inline bool isOnWall(std::vector<std::string> board, Point& p) {
             return board[(int)p.getY()][(int)p.getX()] == '#';
         }
 
+        // Returns true if the point is beyond the board's boundaries, and false otherwise.
         inline bool isBeyondBoundaries(std::vector<std::string> board, Point& p) {
             return p.getY() < 0 || p.getX() < 0 || p.getY() >= board.size() || p.getX() >= board[0].size();
         }
@@ -32,6 +39,9 @@ class MovingEntity {
         inline void setSpeed(double value) { this->speed = value; };
         inline void setCurrentDirection(Direction value) { this->current_moving_direction = value; };
         inline void setCharToPrint(char value) { this->char_to_print = value; };
+        
+        // Receives the board.
+        // Moves the entity to a new location (according to it's getNewPosition implementation) on screen
         void move(Board&);
 };
 
