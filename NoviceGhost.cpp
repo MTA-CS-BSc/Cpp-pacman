eeb1ab651394d861de7e0d6ad7e76e352ee36c5a
Maya Raskin <mayaraskin14@gmail.com>
2023-05-19 21:30:02 +0300

    Changed to > instead of >= on moves amount

--- NoviceGhost.cpp
+++ NoviceGhost.cpp
@@ -1,7 +1,7 @@
 #include "NoviceGhost.h"
 
 bool NoviceGhost::shouldChangeDirection(Board& board, Point& p) {
-	return this->isBeyondBoundaries(board, p) || this->isOnWall(board, p) || this->moves_in_same_direction >= Settings::GHOST_MAX_MOVES_IN_SAME_DIR;
+	return this->isBeyondBoundaries(board, p) || this->isOnWall(board, p) || this->moves_in_same_direction > Settings::GHOST_MAX_MOVES_IN_SAME_DIR;
 }
 
 void NoviceGhost::ghostMovomentLogic(Board& board, Point& p) {
