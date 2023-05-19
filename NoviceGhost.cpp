72b3e475dfdebee7aeb42557ad7a55c980ff31a7
Maya Raskin <mayaraskin14@gmail.com>
2023-05-20 01:19:51 +0300

    Fixed typo in function name

--- NoviceGhost.cpp
+++ NoviceGhost.cpp
@@ -4,7 +4,7 @@ bool NoviceGhost::shouldChangeDirection(Board& board, Point& p) {
 	return this->isBeyondBoundaries(board, p) || this->isOnWall(board, p) || this->moves_in_same_direction > Settings::GHOST_MAX_MOVES_IN_SAME_DIR;
 }
 
-void NoviceGhost::ghostMovomentLogic(Board& board, Point& p) {
+void NoviceGhost::ghostMovementLogic(Board& board, Point& p) {
 	if (shouldChangeDirection(board, p)) {
 		Direction new_direction = this->getRandomDirection();
 
--- NoviceGhost.h
+++ NoviceGhost.h
@@ -4,8 +4,9 @@
 class NoviceGhost : public BaseGhost {
 private:
 	int moves_in_same_direction;
-	bool shouldChangeDirection(Board&, Point&);
-	void ghostMovomentLogic(Board&, Point&);
+
+	bool shouldChangeDirection(Board&, Point&) override;
+	void ghostMovementLogic(Board&, Point&) override;
 
 public:
 	NoviceGhost() : BaseGhost(), moves_in_same_direction(0) { }
