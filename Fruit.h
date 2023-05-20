77f8ec6c31737092a67c8bfc0ad161b6621c2574
Maya Raskin <mayaraskin14@gmail.com>
2023-05-20 20:34:40 +0300

    Added usage of random fruit char func

--- Fruit.h
+++ Fruit.h
@@ -9,9 +9,9 @@ private:
 	bool shouldChangeDirection(Board&, Point&) override;*/
 
 public:
-	Fruit() : MovingEntity(getRandomDirection(), Settings::FRUIT_SPEED, static_cast<char>(generateRandomNumber(Settings::FRUIT_MIN, Settings::FRUIT_MAX))), is_visible(false) { }
+	Fruit() : MovingEntity(getRandomDirection(), Settings::FRUIT_SPEED, generateRandomFruitChar()), is_visible(false) { }
 	inline bool getIsVisible() { return this->is_visible; }
-	inline bool setIsVisible(bool value) { this->is_visible = value;  }
+	inline void setIsVisible(bool value) { this->is_visible = value;  }
 	inline bool getFruitPoints() { return static_cast<int>(this->getCharToPrint()); }
 	bool shouldDisappear();
 };
