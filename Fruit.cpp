30d260a3adc5efbf0aed0e2e4ee44aed43e29cb2
Maya Raskin <mayaraskin14@gmail.com>
2023-05-20 21:31:10 +0300

    Added shouldChangeVisibility call on getNewPosition fruit

--- Fruit.cpp
+++ Fruit.cpp
@@ -6,14 +6,18 @@ bool Fruit::shouldChangeDirection(Board& board, Point& p) {
 }
 
 Point Fruit::getNewPosition(Board& board) {
+	if (shouldChangeVisibility()) {
+		this->setIsVisible(!this->getIsVisible());
+		this->setCharToPrint(board.board_obj[(int)this->getCurrentPosition().getY()][(int)this->getCurrentPosition().getX()]);
+		printAtXY(this->getCurrentPosition().getX(), this->getCurrentPosition().getY(), this->getCharToPrint());
+	}
+
 	Point p = this->current_position;
 
 	p.changeWithDirection(this->getDirection(), this->getSpeed());
 
 	if (shouldChangeDirection(board, p))
 		this->setCurrentDirection(getValidRandomDirection(board, p));
-
-	// TODO: handle random change visibility
 
 	return p;
 }
