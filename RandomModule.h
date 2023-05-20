474f45500c9679f26303ea608576fe899cce6fee
Maya Raskin <mayaraskin14@gmail.com>
2023-05-20 20:34:11 +0300

    Added getRandomChar for fruit

--- RandomModule.h
+++ RandomModule.h
@@ -1,8 +1,13 @@
 #pragma once
 #include <cstdlib>
 #include <ctime>
+#include "Settings.h"
 
 // Returns a random number between start and end
 inline int generateRandomNumber(int start, int end) {
     return start + std::rand() % (end - start + 1);
+}
+
+inline char generateRandomFruitChar() {
+    return static_cast<char>(generateRandomNumber(Settings::FRUIT_MIN, Settings::FRUIT_MAX));
 }
